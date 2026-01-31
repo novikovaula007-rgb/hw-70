057d97d4eb9917e3bac8416fbfc708bd75f07752
ulyana <novikova.ula.007@gmail.com>
2026-01-31 09:25:22 +0300

    fetch contacts action

--- src/features/ContactsSlice/ContactsSlice.ts
+++ src/features/ContactsSlice/ContactsSlice.ts
@@ -1,6 +1,7 @@
-import type {IContact, IContactInList} from "../../types";
-import {createSlice} from "@reduxjs/toolkit";
+import type {IContact, IContactAPI, IContactInList} from "../../types";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import type {RootState} from "../../app/store.ts";
+import {axiosAPI} from "../../axiosAPI.ts";
 
 interface ContactsState {
     contacts: IContactInList[],
@@ -22,15 +23,48 @@ const initialState: ContactsState = {
     }
 }
 
+const fetchContacts = createAsyncThunk<IContactInList[]>(
+    'contacts/FetchContacts',
+    async () => {
+        const response = await axiosAPI.get<IContactAPI | null>('contacts');
+        const contactsData = response.data;
+        if (!contactsData) {
+            return []
+        } else {
+            return Object.keys(contactsData).map(key => {
+                const contact = contactsData[key];
+                return {
+                    name: contact.name,
+                    id: key,
+                    photo: contact.photo
+                }
+            });
+        }
+    }
+)
+
 export const ContactsSlice = createSlice({
     name: 'contactsApp',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchContacts.pending, (state) => {
+                state.loading.loadingFetchContacts = true;
+            })
+            .addCase(fetchContacts.fulfilled, (state, action) => {
+                state.contacts = action.payload;
+                state.loading.loadingFetchContacts = false;
+            })
+            .addCase(fetchContacts.rejected, (state) => {
+                state.loading.loadingFetchContacts = false;
+            })
+    }
 })
 
 export const selectContacts = (state: RootState) => state.contactsApp.contacts;
 export const selectSelectedContact = (state: RootState) => state.contactsApp.selectedContact;
 export const selectContactsLoading = (state: RootState) => state.contactsApp.loading;
 
-export const ContactsReducer =ContactsSlice.reducer;
+export const ContactsReducer = ContactsSlice.reducer;
 
